f7e82045c5ffa0acd7a8ad69086b449139c0391c
PAUL-V-STEP <p.v.stepanov@hotmail.com>
2024-10-03 20:00:42 +0300

    Add files via upload
    Домашнее задание по модулю  1.4

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# Module 1.4
+my_string = input("Введите Вашу строку: ")
+print("Длина строки = ",len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0:1])
+print(my_string[-1])
